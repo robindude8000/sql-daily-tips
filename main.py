02606b2de204fc47ef1b1276e3e3b9c3565e344c
robindude8000 <robindude8000>
2025-07-08 17:55:25 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+client = OpenAI(
+    base_url=os.getenv("OPENROUTER_BASE_URL"),
+    api_key=os.getenv("OPENROUTER_API_KEY")
+)
+
+completion = client.chat.completions.create(
+    model="deepseek/deepseek-r1-0528:free",
+    messages=[
+        {
+          "role": "user",
+          "content": "Give me a short random trivia. Summarize it using non-technical words but with simple to intermediate vocabulary"
+        }
+    ]
+)
+
+trivia_content = completion.choices[0].message.content
+
+print(trivia_content)
